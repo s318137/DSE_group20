b1126fa855bab4919ae92592197eedb7792e4a49
s318137 <128319471+s318137@users.noreply.github.com>
2023-04-25 21:40:38 +0200

    Working as Intended with an handful of tricks

--- LAB5/Part3/Part3.vhdl
+++ LAB5/Part3/Part3.vhdl
@@ -21,9 +21,10 @@ ARCHITECTURE Behavior OF part3 IS
 	END COMPONENT;
 
 	-- declare signals
-	SIGNAL W : STD_LOGIC;
-	SIGNAL OneHot : STD_LOGIC_VECTOR(8 DOWNTO 0);
-	SIGNAL A_up : STD_LOGIC := '1';
+	SIGNAL W : STD_LOGIC := '0';
+	SIGNAL OneHot : STD_LOGIC_VECTOR(8 DOWNTO 0) := "000000000";
+	SIGNAL OneHotNext : STD_LOGIC_VECTOR(8 DOWNTO 0) := "000000000";
+	SIGNAL LEDR_out : STD_LOGIC_VECTOR(9 DOWNTO 0) := "0000000000";
 	SIGNAL KEY1 : STD_LOGIC;
 	SIGNAL Z_out : STD_LOGIC;
 	
@@ -33,110 +34,116 @@ ARCHITECTURE Behavior OF part3 IS
 	BEGIN
 
 		W <= SW(1);
-		A_up <= not(OneHot(0));
 		KEY1 <= KEY(1);
-
-
+	
+	
 		PROCESS (W, Y_Q) -- state table
 			BEGIN
 			
 				CASE Y_Q IS
-					WHEN A => IF (W = '0') THEN 
+					WHEN A => 
+					IF (W = '0') THEN 
 						Y_D <= B;
-						OneHot <= "000000011";
+						OneHotNext <= "000000011";
 					ELSE 
 						Y_D <= F;
-						OneHot <= "000100001";
+						OneHotNext <= "000100001";
 					END IF;
 					
 					WHEN B => IF (W = '0') THEN 
 						Y_D <= C;
-						OneHot <= "000000101";
+						OneHotNext <= "000000101";
 					ELSE 
 						Y_D <= F;
-						OneHot <= "000100001";
+						OneHotNext <= "000100001";
 					END IF;
 					
 					WHEN C => IF (W = '0') THEN 
 						Y_D <= D;
-						OneHot <= "000001001";
+						OneHotNext <= "000001001";
 					ELSE 
 						Y_D <= F;
-						OneHot <= "000100001";
+						OneHotNext <= "000100001";
 					END IF;
 					
 					WHEN D => IF (W = '0') THEN 
 						Y_D <= E;
-						OneHot <= "000010001";
+						OneHotNext <= "000010001";
 					ELSE 
 						Y_D <= F;
-						OneHot <= "000100001";
+						OneHotNext <= "000100001";
 					END IF;
 					
 					WHEN E => IF (W = '0') THEN 
 						Y_D <= E;
-						OneHot <= "000010001";
+						OneHotNext <= "000010001";
 					ELSE 
 						Y_D <= F;
-						OneHot <= "000100001";
+						OneHotNext <= "000100001";
 					END IF;
 					
 					WHEN F => IF (W = '1') THEN 
 						Y_D <= G;
-						OneHot <= "001000001";
+						OneHotNext <= "001000001";
 					ELSE 
 						Y_D <= B;
-						OneHot <= "000000011";
+						OneHotNext <= "000000011";
 					END IF;
 					
 					WHEN G => IF (W = '1') THEN 
 						Y_D <= H;
-						OneHot <= "010000001";
+						OneHotNext <= "010000001";
 					ELSE 
 						Y_D <= B;
-						OneHot <= "000000011";
+						OneHotNext <= "000000011";
 					END IF;
 					
 					WHEN H => IF (W = '1') THEN 
 						Y_D <= I;
-						OneHot <= "100000011";
+						OneHotNext <= "100000011";
 					ELSE 
 						Y_D <= B;
-						OneHot <= "000000011";
+						OneHotNext <= "000000011";
 					END IF;
 					
 					WHEN I => IF (W = '1') THEN 
 						Y_D <= I;
-						OneHot <= "100000001";
+						OneHotNext <= "100000001";
 					ELSE 
 						Y_D <= B;
-						OneHot <= "000000011";
+						OneHotNext <= "000000011";
 					END IF;
 				
 				-- other states
+				
 				END CASE;
 		END PROCESS; -- state table
-
+		
 		PROCESS (KEY1) -- state flip-flops
 			BEGIN
-	
-				fA : flipflop PORT MAP(D => A_up, Clock => KEY1, Resetn => '1', Q => OneHot(0));
-				fB : flipflop PORT MAP(D => OneHot(1), Clock => KEY1, Resetn => SW(0), Q => OneHot(1));
-				fC : flipflop PORT MAP(D => OneHot(2), Clock => KEY1, Resetn => SW(0), Q => OneHot(2));
-
-				fD : flipflop PORT MAP(D => OneHot(3), Clock => KEY1, Resetn => SW(0), Q => OneHot(3));
-				fE : flipflop PORT MAP(D => OneHot(4), Clock => KEY1, Resetn => SW(0), Q => OneHot(4));
-				fF : flipflop PORT MAP(D => OneHot(5), Clock => KEY1, Resetn => SW(0), Q => OneHot(5));
-
-				fG : flipflop PORT MAP(D => OneHot(6), Clock => KEY1, Resetn => SW(0), Q => OneHot(6));
-				fH : flipflop PORT MAP(D => OneHot(7), Clock => KEY1, Resetn => SW(0), Q => OneHot(7));
-				fI : flipflop PORT MAP(D => OneHot(8), Clock => KEY1, Resetn => SW(0), Q => OneHot(8));
-
+				IF (KEY1'EVENT AND KEY1 = '1') THEN
+					Y_Q <= Y_D;
+				END IF; 
 		END PROCESS;
+		
+		-- component instantation
+		fA : flipflop PORT MAP(D => OneHotNext(1), Clock => KEY1, Resetn => SW(0), Q => OneHot(0));
+		fB : flipflop PORT MAP(D => OneHotNext(1), Clock => KEY1, Resetn => SW(0), Q => OneHot(1));
+		fC : flipflop PORT MAP(D => OneHotNext(2), Clock => KEY1, Resetn => SW(0), Q => OneHot(2));
 
-	Z_out <= '1' WHEN (OneHot = "000010001" OR OneHot = "100000001") ELSE '1';
+		fD : flipflop PORT MAP(D => OneHotNext(3), Clock => KEY1, Resetn => SW(0), Q => OneHot(3));
+		fE : flipflop PORT MAP(D => OneHotNext(4), Clock => KEY1, Resetn => SW(0), Q => OneHot(4));
+		fF : flipflop PORT MAP(D => OneHotNext(5), Clock => KEY1, Resetn => SW(0), Q => OneHot(5));
 
-	LEDR <= "0000000001" WHEN (OneHot = "000010001" OR OneHot = "100000001") ELSE "0000000000";
+		fG : flipflop PORT MAP(D => OneHotNext(6), Clock => KEY1, Resetn => SW(0), Q => OneHot(6));
+		fH : flipflop PORT MAP(D => OneHotNext(7), Clock => KEY1, Resetn => SW(0), Q => OneHot(7));
+		fI : flipflop PORT MAP(D => OneHotNext(8), Clock => KEY1, Resetn => SW(0), Q => OneHot(8));
+		
+
+	Z_out <= '1' WHEN (Y_Q = E OR Y_Q = I) ELSE '0';
+
+	LEDR_out(0) <= '1' WHEN (Y_Q = E OR Y_Q = I) ELSE '0';
+	LEDR <= LEDR_out;
 	Z <= Z_out; 
 
 END Behavior;
